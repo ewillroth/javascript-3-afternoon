4b3be9c98a785f94e94e62a84e1870e6f1189cca
Eric Willroth <ewillroth@gmail.com>
2018-11-28 14:17:25 -0600

    complete classes

--- classes.js
+++ classes.js
@@ -1,13 +1,4 @@
 /* 
-  Once you complete a problem, refresh ./classes.html in your browser and check to see if the problem's test(s) are passing.
-  Passed tests will be indicated by a green circle.
-  Failed tests will be indicated by a red X.
-
-  You can refresh the page at any time to re-run all the tests.
-
-  Classes are a tool for building similar objects over and over again.
-  They are a construct that helps your organize your code.
-
   Let's work with some employees at a company.
   You work for Widget Co. They have hundreds of employees.
 */
@@ -29,9 +20,17 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
-
-
+class Employee {
+	constructor(first_name, last_name, email, age) {
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.email = email;
+		this.age = age;
+	}
+	makeWidget() {
+		return `${this.first_name} ${this.last_name} Widget`;
+	}
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -49,9 +48,24 @@
   Call your new class Manager
 */
 
-//Code Here
-
-
+class Manager {
+	constructor(first_name, last_name, email, age) {
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.email = email;
+		this.age = age;
+		this.reports = [];
+	}
+	makeWidget() {
+		return `${this.first_name} ${this.last_name} Widget`;
+	}
+	hire(employee) {
+		this.reports.push(employee);
+	}
+	fire(index) {
+		this.reports.splice(index, 1);
+	}
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -75,9 +89,49 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
-
-
+class ProgressiveManager {
+	constructor(first_name, last_name, email, age) {
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.email = email;
+		this.age = age;
+		this.reports = [];
+		this.title = 'Not a manager';
+		this.bonus = 0;
+	}
+	makeWidget() {
+		return `${this.first_name} ${this.last_name} Widget`;
+	}
+	hire(employee) {
+		this.reports.push(employee);
+		this.reports.length === 0
+			? (this.title = 'Not a manager')
+			: this.reports.length < 3
+			? (this.title = 'Barely Manager')
+			: this.reports.length < 10
+			? (this.title = 'Mostly Manager')
+			: this.reports.length < 50
+			? (this.title = 'Manager')
+			: this.reports.length < 100
+			? (this.title = 'Manager Plus')
+			: (this.title = 'Bestest Manager');
+	}
+	fire(index) {
+		this.reports.splice(index, 1);
+		this.bonus += 100;
+		this.reports.length === 0
+			? (this.title = 'Not a manager')
+			: this.reports.length < 3
+			? (this.title = 'Barely Manager')
+			: this.reports.length < 10
+			? (this.title = 'Mostly Manager')
+			: this.reports.length < 50
+			? (this.title = 'Manager')
+			: this.reports.length < 100
+			? (this.title = 'Manager Plus')
+			: (this.title = 'Bestest Manager');
+	}
+}
 
 ////////// PROBLEM 4 - Black Diamond //////////
 
@@ -102,6 +156,26 @@
         - The anonymous function should decrease wear_and_tear_count by 10, and set needs_reboot to false
 */
 
-//Code Here
-
-
+class Machine {
+	constructor() {
+		this.widgets_made_count = 0;
+		this.wear_and_tear_count = 0;
+		this.needs_reboot = false;
+	}
+	makeWidgets(number) {
+		this.widgets_made_count += number;
+		this.wear_and_tear_count += number / 50;
+	}
+	fixMachine() {
+		this.needs_reboot = true;
+	}
+	reboot() {
+		return () => {
+			this.wear_and_tear_count -= 10;
+			this.needs_reboot = false;
+		};
+	}
+	rebootComplete() {
+		this.reboot();
+	}
+}
