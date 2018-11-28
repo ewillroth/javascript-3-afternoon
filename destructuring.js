4afa908206d0e05362f6c4cb3fd9b74f0b22ed09
Eric Willroth <ewillroth@gmail.com>
2018-11-28 13:43:44 -0600

    complete destructuring

--- destructuring.js
+++ destructuring.js
@@ -1,29 +1,19 @@
-/*
-  Once you complete a problem, refresh ./destructuring.html in your browser and check to see if the problem's test(s) are passing.
-  Passed tests will be indicated by a green circle.
-  Failed tests will be indicated by a red X.
-
-  You can refresh the page at any time to re-run all the tests.
-*/
-
 ////////// PROBLEM 1 //////////
 
 // Do not edit the code below.
 var carDetails = {
-  color: 'red',
-  make: 'toyota',
-  model: 'tacoma',
-  year: 1994
-}
+	color: 'red',
+	make: 'toyota',
+	model: 'tacoma',
+	year: 1994
+};
 // Do not edit the code above.
 
 /*
   Use object destructuring to save the property values from the object carDetails into new variables. 
 */
 
-//Code Here
-
-
+var { color, make, model, year } = carDetails;
 
 ////////// PROBLEM 2 //////////
 
@@ -33,15 +23,12 @@ var carDetails = {
   The property names are firstName, lastName, and title.
 */
 
-function greeting( obj ) {
-  //Code Here
-  
-  // Do not edit the code below.
-  return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
-  // Do not edit the code above.
+function greeting(obj) {
+	var { firstName, lastName, title } = obj;
+	// Do not edit the code below.
+	return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
+	// Do not edit the code above.
 }
-
-
 
 ////////// PROBLEM 3 //////////
 
@@ -53,9 +40,10 @@ function greeting( obj ) {
   Sum up the values and return the total number.
 */
 
-//Code Here
-
-
+function totalPopulation(obj) {
+	var { utah, california, texas, arizona } = obj;
+	return utah + california + texas + arizona;
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -67,9 +55,10 @@ function greeting( obj ) {
   Push these new variables to an array and return the array. 
 */
 
-//Code Here
-
-
+function ingredients(obj) {
+	var { carb, fat, protein } = obj;
+	return [carb, fat, protein];
+}
 
 ////////// PROBLEM 5 //////////
 
@@ -85,9 +74,15 @@ function greeting( obj ) {
   Find the smallest number of the three and return that number.
 */
 
-//Code Here
-
-
+function largeNumbers({ first, second, third }) {
+	if (first < second && first < third) {
+		return first;
+	} else if (second < first && second < third) {
+		return second;
+	} else {
+		return third;
+	}
+}
 
 ////////// PROBLEM 6 //////////
 
@@ -97,6 +92,12 @@ function greeting( obj ) {
   Find the longest array and return that array.
 */
 
-//Code Here
-
-
+function numberGroups({ a, b, c }) {
+	if (a.length > b.length && a.length > c.length) {
+		return a;
+	} else if (b.length > a.length && b.length > c.length) {
+		return b;
+	} else {
+		return c;
+	}
+}
